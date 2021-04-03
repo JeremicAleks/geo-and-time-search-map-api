7dc50cdcb57a7f1431c13a3ebf3bb332d2572e5e
JeremicAleks <jeremicaleksandar96@gmail.com>
2021-04-03 16:12:07 +0200

    Init spring project

---
+++ src/test/java/com/ftn/master/geoandtimesearchmapapi/GeoAndTimeSearchMapApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftn.master.geoandtimesearchmapapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoAndTimeSearchMapApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
